e7f999ea90a9c78c0fd78edd56603eacc1709e3d
tianvincent0 <tianvincent0@gmail.com>
2025-07-15 16:18:57 +0800

    fix hilt issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
-    implementation(libs.androidx.room.common.jvm)
     kapt("com.google.dagger:hilt-compiler:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
